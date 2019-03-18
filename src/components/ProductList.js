dbbe6c9118c56bc5b8bec09d169c898a5e224342
Cesar Jimenez <cijgr@hotmail.com>
2019-03-17 23:36:26 -0600

    Project structure and navbar ready

---
+++ src/components/ProductList.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import Product from "./Product";
+
+export default class ProductList extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Hello from Product List</h3>
+      </div>
+    );
+  }
+}
